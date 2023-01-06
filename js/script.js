1dbea3843e2ef661069e967e67d0b1f5d843a259
aboab <111141901+KonstantinRevin@users.noreply.github.com>
2023-01-06 21:51:49 +0200

    1
    1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+swiper = new Swiper('.slider', {
+    // // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+  
+    // // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',  
+    //   clicable: true
+    },
+    
+    direction: 'vertical',
+    loop: true ,
+    slidesPerView: 1,
+    clicable: true,
+
+    // // clicable: true
+   
+  });
+
+ 
